33641b31c1078169906b31c5351eac70dba9af91
Freddy <SEOWFREDDY.23@ichat.sp.edu.sg>
2024-11-03 15:53:43 +0800

    updated bmi return

--- bmi.py
+++ bmi.py
@@ -10,7 +10,7 @@ def calculate_bmi(height, weight):
  print("You are "+classification)
  
  if bmi < 18.5 : return -1
- if bmi > 25.0: return 1
+ elif bmi > 25.0: return 1
  else: return 0
 
 print(calculate_bmi(1.73, 120))
